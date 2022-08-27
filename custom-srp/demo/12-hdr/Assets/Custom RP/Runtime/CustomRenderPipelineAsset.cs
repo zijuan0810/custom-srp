09c8e6e68490e74bdc8c50908a4b2359b74b7249
jagersong <jagersong@163.com>
2022-08-27 21:58:04 +0800

    12-hdr

---
+++ custom-srp/demo/12-hdr/Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset {
+
+	[SerializeField]
+	bool
+		useDynamicBatching = true,
+		useGPUInstancing = true,
+		useSRPBatcher = true,
+		useLightsPerObject = true;
+
+	[SerializeField]
+	ShadowSettings shadows = default;
+
+	[SerializeField]
+	PostFXSettings postFXSettings = default;
+
+	protected override RenderPipeline CreatePipeline () {
+		return new CustomRenderPipeline(
+			useDynamicBatching, useGPUInstancing, useSRPBatcher,
+			useLightsPerObject, shadows, postFXSettings
+		);
+	}
+}
---
+++ custom-srp/demo/12-hdr/Assets/Custom RP/Runtime/PostFXSettings.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Post FX Settings")]
+public class PostFXSettings : ScriptableObject {
+
+	[SerializeField]
+	Shader shader = default;
+
+	[System.Serializable]
+	public struct BloomSettings {
+
+		[Range(0f, 16f)]
+		public int maxIterations;
+
+		[Min(1f)]
+		public int downscaleLimit;
+
+		public bool bicubicUpsampling;
+
+		[Min(0f)]
+		public float threshold;
+
+		[Range(0f, 1f)]
+		public float thresholdKnee;
+
+		[Min(0f)]
+		public float intensity;
+	}
+
+	[SerializeField]
+	BloomSettings bloom = default;
+
+	public BloomSettings Bloom => bloom;
+
+	[System.NonSerialized]
+	Material material;
+
+	public Material Material {
+		get {
+			if (material == null && shader != null) {
+				material = new Material(shader);
+				material.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return material;
+		}
+	}
+}
